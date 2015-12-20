a761c8c763e157aa59f61e94a773720ddf585cbd
BIGOplO <ll33457791@gmail.com>
2015-12-21 02:30:17 +0800

    Shopping list

---
+++ TeamProject/shoppinglist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    class shoppinglist
+    {
+       shopping_cart sc;
+       public void PrintList()
+        {
+            sc.Result();
+            System.Console.WriteLine("The total price is ");
+            System.Console.WriteLine(sc.PriceSum);
+        }
+
+    }
+}
