d7b2225a54208c09fad74436a1f868ed5270b446
tina <t02315t02315@gmail.com>
2015-12-27 13:49:28 +0800

    修正
    test

--- TeamProject/shopping cart.cs
+++ TeamProject/shopping cart.cs
@@ -59,19 +59,20 @@ namespace TeamProject
             want.Clear();
         }
         public int PriceSum { get { return PriceSum_; } }
-        public void Result()
+        public string Result()
         {
             string s = "";
             s += "購物車\n" +
                  "\n";
-            int count = 1;
             s += string.Format("{0}\t{1}\t{2}\tx\t{3}\t=\t{4}\n", "項次", "品名", "價格","數量", "金額");
             for (int i = 0; i < want.Count; i++)
             {   
                 int m = want[i].price * ngood[i];
-                s += string.Format("{0}\t{1}\t{2}\tx\t{3}\t=\t{4}\n", i, want[i].name, want[i].price, ngood[i], m);
+                s += string.Format("{0}\t{1}\t{2}\tx\t{3}\t=\t{4}\n", i+1, want[i].name, want[i].price, ngood[i], m);
             }
-            s += string.Format("{0}\t:{1}", "總金額", PriceSum_);
+            s += "\n";
+            s += string.Format("{0}\t:\t{1}", "總金額", PriceSum_);
+            return s;
         }
         private int PriceSum_ = 0;
     }
