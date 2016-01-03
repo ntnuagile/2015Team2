2bf7cf0f8f1a841e59125c1c003103168312beed
BIGOplO <ll33457791@gmail.com>
2016-01-04 02:45:25 +0800

    final

---
+++ TeamProject/ShopingLists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    class ShopingLists
+    {
+        public void PrintList(shopping_cart sc)
+        {
+            System.Console.WriteLine(sc.Result());
+        }
+    }
+}
